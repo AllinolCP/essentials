854ee719d8ab7e3b59aa76b82cd879608f6a45fd
AllinolCP <56921534+AllinolCP@users.noreply.github.com>
2020-06-02 16:22:52 +0700

    Update __init__.py
    Added ban, Kick, add furniture

--- essentials/__init__.py
+++ essentials/__init__.py
@@ -1,9 +1,9 @@
 from houdini.plugins import IPlugin
 from houdini import commands
-
+from houdini.data.penguin import Penguin
 from houdini import permissions
 from houdini.data.room import Room
-
+from houdini.handlers.play.moderation import moderator_ban,moderator_kick
 import difflib
 
 
@@ -21,6 +21,9 @@ class Essentials(IPlugin):
         await self.server.permissions.register('essentials.jr')
         await self.server.permissions.register('essentials.ai')
         await self.server.permissions.register('essentials.ac')
+        await self.server.permissions.register('essentials.af')
+        await self.server.permissions.register('essentials.ban')
+        await self.server.permissions.register('essentials.kick')
 
         self.items_by_name = {item.name: item for item in self.server.items.values()}
 
@@ -47,6 +50,33 @@ class Essentials(IPlugin):
             await p.add_inventory(item, cost=0)
         except (IndexError, KeyError):
             await p.send_xt('mm', 'Item does not exist', p.id)
+            
+    @commands.command('af')
+    @permissions.has_or_moderator('essentials.af')
+    async def add_furniture(self, p, furniture: int):
+        await p.add_furniture(p.server.furniture[furniture], notify=True, cost=0)
+    
+    @commands.command('ban')
+    @permissions.has_or_moderator('essentials.ban')
+    async def ban_penguin(self, p, player: str, message: str, duration: int):
+      try:
+        player = player.lower()
+        penguin_id = await Penguin.select('id').where(Penguin.username == player).gino.first()
+        penguin_id = int(penguin_id[0])
+        await moderator_ban(p, penguin_id, hours=duration, comment=message)    
+      except (StopIteration):
+        await p.send_xt('mm', 'You need to specify a reason!', p.id)
+
+    @commands.command('kick')
+    @permissions.has_or_moderator('essentials.kick')
+    async def kick_penguin(self, p, player: str):
+        try:
+            player = player.lower()
+            penguin_id = await Penguin.select('id').where(Penguin.username == player).gino.first()
+            penguin_id = int(penguin_id[0])
+            await moderator_kick(p, penguin_id)
+        except (StopIteration):
+            await p.send_xt('mm', 'You need to specify a Player!', p.id)
 
     @commands.command('ac')
     @permissions.has_or_moderator('essentials.ac')
